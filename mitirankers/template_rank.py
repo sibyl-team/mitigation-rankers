7fe358b5369b8890a3b97e4cc2928ea248de6f1e
Fabio Mazza <fab4mazz@gmail.com>
2022-07-05 16:02:14 +0200

    Define abstracts

--- mitirankers/template_rank.py
+++ mitirankers/template_rank.py
@@ -1,15 +1,18 @@
+import abc
+import numpy as np
 ## TEMPLATE for the class of the algorithm that rank indivuals to be tested
 
-class AbstractRanker():
+class AbstractRanker:
 
     def __init__(self):
         self.description = "This shape has not been described yet"
         self.author = "Nobody has claimed to make this shape yet"
         self.rng = np.random.RandomState(1)
-
+    @abc.abstractmethod
     def init(self, N, T):
         raise NotImplementedError
-        
+    
+    @abc.abstractmethod
     def rank(self, t, daily_contacts, daily_obs, data):
         '''
         Order the individuals by the probability to be infected
@@ -29,3 +32,6 @@ class AbstractRanker():
         list -- [(index, value), ...]
         '''
         raise NotImplementedError
+
+    def set_seed(self,seed):
+        self.rng = np.random.RandomState(np.random.PCG64(seed))
