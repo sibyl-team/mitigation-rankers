c106ebba845d24c1720ecc8f8f27c5720c6be93d
Fabio Mazza <fab4mazz@gmail.com>
2022-09-22 17:29:31 +0200

    faster DCT

--- mitirankers/dct_rank.py
+++ mitirankers/dct_rank.py
@@ -4,16 +4,21 @@ import numpy as np
 import pandas as pd
 from .template_rank import AbstractRanker
 
+dtype_obs = np.dtype([(k,'int') for k in ("i","s","t_test")])
 
 class DctRanker(AbstractRanker):
 
     def __init__(self,
                 tau = 7,
-                seed = 1): # time-window of seven days
+                seed = 1,
+                noise=1e-3,
+                debug=False): # time-window of seven days
         self.description = "class for naif contact tracing"
         self.tau = tau
         self.rng = np.random.RandomState(seed)
+        self.noise=noise
 
+        self.debug=debug
     def init(self, N, T):
         self.contacts = None
         self.N = N
@@ -25,8 +30,8 @@ class DctRanker(AbstractRanker):
     def _save_contacts(self, daily_contacts):
         """
         Save contacts using numpy named array
-        Seems more difficult but it's not,
-        and much faster than using pandas dataframes (10x)
+        A little more complicated, but
+        much faster than using pandas dataframes (10x)
 
         Assuming contacts are tuple (i,j,t,lambda) in this order
         @author Fabio Mazza
@@ -57,40 +62,51 @@ class DctRanker(AbstractRanker):
         computing rank of infected individuals
         return: list -- [(index, value), ...]
         '''
-        
+        tstart=time.time()
         for obs in daily_obs:
-            self.obs.append(obs)
+            self.obs.append(tuple(obs))
         
         self._clear_old_contacts(t_day, self.tau)
         #for (i,j,t,l) in daily_contacts:
         #    self.contacts.append([i,j,t,l])
         self._save_contacts(daily_contacts)
-
-        obs_df = pd.DataFrame(self.obs, columns=["i", "s", "t_test"])
+        t_c_o = time.time()-tstart
+        if self.debug: print(f"Saving cts and obs: {t_c_o:4.3f} s")
+        t0 = time.time()
+        obs_df = np.array(self.obs, dtype=dtype_obs)
+        #obs_df=pd.DataFrame(self.obs, columns=["i", "s", "t_test"])
         #obs_df = obs_df[obs_df["t_test"] >= t_day - self.tau]
-        contacts_df = pd.DataFrame({k: self.contacts[k] for k in ("i", "j", "t", "lambda")})
-        contacts_df = contacts_df[contacts_df["t"] >= t_day - self.tau]
-
-        print("num of contacts: ", len(contacts_df), end="; ")
+        contacts = self.contacts #pd.DataFrame({k: self.contacts[k] for k in ("i", "j", "t", "lambda")})
+        contacts = contacts[contacts["t"] >= t_day - self.tau]
+        t_df = time.time()-t0
+        print("num of contacts: ", len(contacts), end="; ")
         print("num of obs: ", len(obs_df), end="; ")
+        if self.debug: print(f"time for df: {t_df:4.3f}")
         
+        noise=self.noise
+
         #Score = dict([(i, 0) for i in range(self.N)])
         t0 = time.time()
-        Score = pd.Series(np.zeros(self.N))
-        idx_I = obs_df[obs_df["s"] == 1]["i"].to_numpy()
-        idx_I_today = obs_df[(obs_df["s"] == 1) & (obs_df["t_test"] == t_day-1)]["i"].to_numpy()
-        idx_dct = contacts_df[contacts_df["i"].isin(idx_I_today)]["j"].to_numpy()
-        #print(len(idx_I_today), len(idx_I))
-        for i in idx_I:
-            Score[i] = self.N + 1e-3 * self.rng.rand()
-        for i in idx_dct:
-            Score[i] = 1.0 + 1e-3 * self.rng.rand()
-        tend = time.time() - t0
+        Score = run_dct(self.N, contacts, obs_df, t_day, self.rng, noise)
+        tend = (time.time() - t0) if self.debug else (time.time() - tstart)
         print(f"Done. Took {tend:4.3f} s")
         score_s = Score.sort_values(ascending=False)
         score_s = list(zip(score_s.index, score_s.values))
         #sorted_Score = list(sorted(Score.items(), key=lambda item: item[1], reverse=True))
         return score_s
 
-
-
+def run_dct(N, contacts, observ, t_day, rng, noise):
+    Score = pd.Series(np.zeros(N))
+    idx_I = observ[observ["s"] == 1]["i"] #.to_numpy()
+    idx_I_today = observ[(observ["s"] == 1) & (observ["t_test"] == t_day-1)]["i"] #.to_numpy()
+    idx_dct = contacts[np.isin(contacts["i"],idx_I_today)]["j"] #.to_numpy()
+    #print(len(idx_I_today), len(idx_I))
+    #for i in idx_I:
+    #    Score[i] = self.N + noise * self.rng.rand()
+    idc = np.unique(idx_I)
+    Score[idc] = N +rng.rand(len(idc))*noise
+    #for i in idx_dct:
+    #    Score[i] = 1.0 + noise * self.rng.rand()
+    idc2 = np.unique(idx_dct)
+    Score[idc2] = 1 + noise*rng.rand(len(idc2))
+    return Score
